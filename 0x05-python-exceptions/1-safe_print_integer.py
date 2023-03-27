f53bcdba646f709312b528b0d597dd3c93673b5f
desxgnerX <117768170+desxgnerX@users.noreply.github.com>
2023-03-27 23:10:02 +0200

    Delete 0x05-python-exceptions directory

--- 0x05-python-exceptions/1-safe_print_integer.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def safe_print_integer(value):
-    try:
-        print("{:d}".format(value), end="")
-        print()
-        return True
-    except(TypeError, ValueError):
-        return False
